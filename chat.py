4723b067b3657b2bdf9571b79480fd0366affa5b
Pmoney1383 <amirparsaaminian1383@yahoo.com>
2026-02-23 11:16:29 -0800

    first chatbot created

---
+++ chat.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+
+MAX_LEN = 20
+VOCAB_SIZE = 5000
+
+# Load model
+model = tf.keras.models.load_model("chatbot_model.keras")
+
+# Load tokenizer
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+index_word = tokenizer.index_word
+word_index = tokenizer.word_index
+
+
+def generate_response(input_text):
+    input_text = input_text.lower()
+
+    # Convert input to sequence
+    input_seq = tokenizer.texts_to_sequences([input_text])
+    input_seq = tf.keras.preprocessing.sequence.pad_sequences(
+        input_seq,
+        maxlen=MAX_LEN,
+        padding='post',
+        truncating='post'
+    )
+
+    # Start token
+    start_token = word_index.get("<start>")
+    end_token = word_index.get("<end>")
+
+    decoder_input = np.zeros((1, MAX_LEN - 1))
+    decoder_input[0, 0] = start_token
+
+    response = []
+
+    for i in range(1, MAX_LEN - 1):
+        prediction = model.predict([input_seq, decoder_input], verbose=0)
+
+        predicted_token = np.argmax(prediction[0, i - 1])
+
+        if predicted_token == end_token:
+            break
+
+        word = index_word.get(predicted_token, "")
+        response.append(word)
+
+        decoder_input[0, i] = predicted_token
+
+    return " ".join(response)
+
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() == "exit":
+            break
+
+        bot_response = generate_response(user_input)
+        print("Aris:", bot_response)
